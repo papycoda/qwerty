703013680a7086df6b13d014c8319ab00ba74c43
papycoda <opeyemi655@gmail.com>
2024-09-02 16:05:33 +0100

    *

--- enyata_api/enyata_api/settings.py
+++ enyata_api/enyata_api/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-=6o%oj8(=#ux2rcdmawfdno)8=5#j@^qaeeg0=+j$((htg%^f8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
