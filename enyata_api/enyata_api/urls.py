e327987ef60bda4ee0eeae3e780c67ea4dab081a
papycoda <opeyemi655@gmail.com>
2024-09-02 16:19:50 +0100

    "Updated imports and URL patterns in urls.py"

--- enyata_api/enyata_api/urls.py
+++ enyata_api/enyata_api/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -34,9 +34,9 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    re_path(r'^$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=300), name='schema-json'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=300), name='schema-redoc'),
 
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
