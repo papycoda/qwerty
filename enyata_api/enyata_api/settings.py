d4e2d11acd9ac292193f312638b19a6f6ad24e0e
papycoda <opeyemi655@gmail.com>
2024-09-02 15:42:21 +0100

    "removed conditional assignment."

--- enyata_api/enyata_api/settings.py
+++ enyata_api/enyata_api/settings.py
@@ -166,9 +166,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = [
-    os.path.join(BASE_DIR, 'staticfiles'),
-]
+STATIC_ROOT = "os.path.join(BASE_DIR, 'staticfiles')"
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
